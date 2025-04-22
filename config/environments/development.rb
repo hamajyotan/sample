233c613f90393f8834b99219fcf7b076febd357d
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2025-04-22 22:51:28 +0900

    avoid blocked host error.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # avoid blocked host error.
+  config.hosts.clear
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
